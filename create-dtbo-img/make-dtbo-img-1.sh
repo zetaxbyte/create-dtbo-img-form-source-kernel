c8489620ec47d09890d8f4e8f3b1e70848303a15
zetaxbyte <zetaxlinux@gmail.com>
2024-06-02 06:35:29 +0000

    add make dtbo.for for dtb

--- create-dtbo-img/make-dtbo-img-1.sh
+++ create-dtbo-img/make-dtbo-img-1.sh
@@ -29,24 +29,4 @@ echo
 fi
 exit
 
-if [ -d ../../out/arch/arm64/boot/dts/qcom ] ; then
-sleep 1
-echo -e "\n$cyan============================"
-echo -e "$cyan MAKE DTBO.IMG FROM DTS ..."
-echo -e "$cyan============================\n"
-sleep 1
-python src/mkdtboimg.py create ../../out/arch/arm64/boot/dtbo.img ../../out/arch/arm64/boot//dts/qcom/*.dts
-sleep 1
-echo -e "\n$green============================"
-echo -e "$green             DONE"
-echo -e "$green============================\n $normal"
-sleep 1
-ls ../../out/arch/arm64/boot
-echo
-sleep 0.5
-du -sh ../../out/arch/arm64/boot/dtbo.img
-echo
-fi
-exit
-
 
